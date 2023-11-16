b5e08eed7f14641da5e77e97b018656da0adb7d5
Omar622 <omaramin622@gmail.com>
2023-11-16 11:52:35 +0200

    Add login & refresh token endpoints

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,17 @@
 from rest_framework.routers import SimpleRouter
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from .views import UserViews
 
 router = SimpleRouter()
 router.register(r'user', UserViews, basename="user")
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
